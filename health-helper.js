184877ea72aeab5203dccfe63156ffd09753df25
gy7559 <62734041+gy7559@users.noreply.github.com>
2022-06-02 13:04:32 +0900

    Update health-helper.js

--- health-helper.js
+++ health-helper.js
@@ -1,3 +1,4 @@
+let dropBox
 let video
 let size=0;
 let poseNet;
@@ -6,32 +7,44 @@ let poses=[];
 //let keypointY=[];
 //let skeletons=[];
 
-let canvas;
-let vid;
-
-
-function gotFile(file){  
-  video = createVideo([file.data], vidLoad)
-  console.log('file: '+file.data)
-  video.size(width,height)
-  
-  poseNet = ml5.poseNet(video,modelReady); 
-  poseNet.on('pose', (results)=>{
-      poses = results
-  });
-}
-
 
 function setup() {
-  canvas = createCanvas(300, 200);  
-  canvas.drop(gotFile)
-  background(150);   
+  dropBox = createCanvas(1240, 1080);
+  background(100)
+  input = createFileInput((file)=>{
+    if(file.type === "video"){
+      video = createVideo(file.data, vidLoad)
+      video.size(width,height)
+      video.hide()
+      dropBox.resize(video.width,video.height)
+      poseNet = ml5.poseNet(video,modelReady); 
+      poseNet.on('pose', (results)=>{
+      poses = results
+      });
+    }
+  })
+  input.position(0,0)
+  //video = createCapture(VIDEO)
+  
+  
+  /*dropBox.drop(afterDrop)*/
+  
 }
 
+
+/*function afterDrop(file){
+  console.log(file)
+  dropBox.hide()
+  video = createVideo(file.data, vidLoad)
+  video.hide()
+  poseNet = ml5.poseNet(video,modelReady);  
+  poseNet.on('pose', gotResult);
+  console.log("posenet excute")
+}*/
 
 function vidLoad(){
   video.loop()
-  video.volume(1)
+  video.volume(0)
   console.log("video ON")
 }
 
@@ -39,12 +52,19 @@ function modelReady(){
   console.log('model OK');
 }
 
+/*function gotResult(results){
+  console.log("results = "+results)
+  poses = results[0].pose.keypoints;
+  skeletons = results[0].skeleton;
+}*/
 function drawKeypoints(){
   for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
     let pose = poses[i].pose;
-    for (let j = 0; j < pose.keypoints.length; j++) {
+    for (let j = 5; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
       let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
          noStroke();
         fill(255, 0, 0);
@@ -54,8 +74,10 @@ function drawKeypoints(){
   }
 }
 function drawSkeleton(){
+    // Loop through all the skeletons detected
   for (let i = 0; i < poses.length; i++) {
     let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
     for (let j = 0; j < skeleton.length; j++) {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
@@ -64,9 +86,23 @@ function drawSkeleton(){
     }
   }
 }
-function draw(){  
-  background(100)
-  drawKeypoints()
-  drawSkeleton()
-  
-}
+function draw(){
+  //imageMode(CORNER)
+  if(video){
+    image(video,0,0,width,height);
+    drawKeypoints()
+    drawSkeleton()  
+  }
+  /*fill('#FFFF00');
+  stroke('#FF0000');
+  strokeWeight(3);
+  for(let i = 5; i < poses.length; i++){
+    keypointX[i]=round(poses[i].position.x);
+    keypointY[i]=round(poses[i].position.y);
+    ellipse(keypointX[i], keypointY[i],10);
+  }
+  stroke('#FFFF00');
+  strokeWeight(2);
+  for(let i=0; i< skeletons.length; i++){
+    line(round(skeletons[i][0].position.x), round(skeletons[i][0].position.y), round(skeletons[i][1].position.x), round(skeletons[i][1].position.y));
+  }*/
